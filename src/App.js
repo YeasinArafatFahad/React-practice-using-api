c36bdfce5254f445064a9771861265d340a7a20a
YeasinArafatFahad <ayeasin90@gmail.com>
2021-02-26 12:06:28 +0600

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  //const player=[{name:'Pele', age:56},{name:'Ronaldo',age:36},{name:'Messi',age:33},{name:'Neymar',age:26},{name:'Salah',age:30}]
+  const[player, setPlayer]=useState([])
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data =>setPlayer(data))
+  },[])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <MovieCounter></MovieCounter>
+        {
+          player.map(players=> <New name={players.name} key={players.id} ></New>)
+        }
       </header>
     </div>
   );
+}
+
+
+
+function MovieCounter(){
+  const [count,setCount]= useState(0)
+  const handleClick = () => setCount(count + 1);
+  return(
+    <div>
+      <h5>Number Of Movies:{count}</h5>
+      <MovieDisplay movies = {count}></MovieDisplay>
+      <MovieDisplay movies = {count + 10}></MovieDisplay>
+      <button onClick={handleClick}>Add Movies</button>
+    </div>
+  )
+}
+
+function MovieDisplay(prop){
+  return(
+    <h4>Movies Acted:{prop.movies}</h4>
+  )
+}
+
+function New(prop){
+  const pl={
+    color:'black',
+    border:'5px solid green',
+    margin:'10px',
+    width:'1000px',
+    borderRadius:'10px',
+    backgroundColor:'cyan'
+  }
+  return(
+    <div style={pl}>
+      <h1>Superstar Footballer-Name:{prop.name}</h1>
+    </div>
+  )
 }
 
 export default App;
